de6324667bdce4af73a12f14e5ae22e19781cb91
Dariusz Czajka <dariusz.czajka@enxoo.com>
2024-03-24 15:11:52 +0100

    commit

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,3 +1,14 @@
+describe('Check carousel', () => {
+  it('Title and desc should be visible for each card in carousel', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-button-next').should('be.visible');
+    cy.get('.swiper-button-prev').should('be.visible');
+    
+    cy.get('.swiper-slide-visible').should('be.visible')
+  });
+});
+
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
     cy.visit('http://localhost:3000');
